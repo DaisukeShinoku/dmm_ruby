5bbfdb090deacefd34f9b316f81752c31fc86f49
DaisukeShinoku <shinoku.daisuke.ycu@gmail.com>
2020-04-17 09:09:05 +0900

    [Start]2-8

---
+++ cp8-2.rb
@@ -0,0 +1,14 @@
+puts "計算をはじめます"
+puts "何回繰り返しますか？"
+x = gets.to_i
+for i in 1..x do
+	puts "#{i}回目の計算"
+	puts "2つの値を入力してください"
+	a = gets.to_i
+	b = gets.to_i
+	puts "a+b = #{a+b}"
+	puts "a-b = #{a-b}"
+	puts "a*b = #{a*b}"
+	puts "a/b = #{a/b}"
+end
+puts "計算を終了します"
